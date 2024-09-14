dd93386b0a603e29427f6b2fa7df8870f0a77b96
HusseinSaeid <55005651+HusseinSaeid@users.noreply.github.com>
2024-09-14 20:45:02 +0300

    3d effect

--- main.js
+++ main.js
@@ -30,3 +30,16 @@ Icon.onclick = function () {
     isUiVisible = false;
   }
 };
+VanillaTilt.init(document.querySelector(".card1"), {
+  max: 25,
+  speed: 400,
+});
+VanillaTilt.init(document.querySelector(".card2"), {
+  max: 25,
+  speed: 400,
+});
+
+VanillaTilt.init(document.querySelector(".card3"), {
+  max: 25,
+  speed: 400,
+});
